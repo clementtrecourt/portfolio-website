62c3680b69399f89bc685ec67d2a9b6f6f9658d2
unknown <clement.trecourt@gmail.com>
2024-06-30 21:59:31 +0200

    Bento Grid

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,7 +1,22 @@
 import React from "react";
-
+import { BentoGrid, BentoGridItem } from "./ui/BentoGrid";
 const Grid = () => {
-  return <section id="about">Grid</section>;
+  return (
+    <section id="about">
+      <BentoGrid>
+        {[{ tiltle: "Title 1", description: "Desc1", id: "1" }].map(
+          (item, i) => (
+            <BentoGridItem
+              id={item.id}
+              key={item.id}
+              title={item.title}
+              description={item.description}
+            />
+          )
+        )}
+      </BentoGrid>
+    </section>
+  );
 };
 
 export default Grid;
